9f3c8897f7cac2c51d7c23068beeb9c09e5ea47a
Kamila <milka107@interia.pl>
2019-10-03 22:11:38 +0200

    Change App from function to class component, add Counter, add state to make Counter change on clicking plus button.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-    return (
-        <div>
-            <Button sign="-"/>
-            <Button sign="+"/>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState((state, props) => ({
+            count: state.count + 1
+          }));
+    }
+    
+    render() {
+        return (
+            <div>
+                <Button sign="-" />
+                <Counter count={this.state.count} />
+                <Button sign="+" handleChange={this.handleChange} />
+            </div>
+        );
+    }
 }
 
 class Button extends Component {
@@ -16,14 +31,33 @@ class Button extends Component {
     }
     render() {
         return (
-            <button>{this.props.sign}</button>
+            <button onClick={this.props.handleChange}>{this.props.sign}</button>
         );
     }
 }
 
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props.count);
+        return (
+            <div>{this.props.count}</div>
+        );
+    }
+}
+
+
+
 ReactDOM.render(
-    <Counter/>,
+    <App/>,
     document.getElementById('root')
 );
 
 export default App;
+
+
+
+// this.props.sign == "+" ? (count: state.count + 1) : (count: state.count - 1)
