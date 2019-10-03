2726f5d8267b69bcef2e9168c3857addbfed5a36
Kamila <milka107@interia.pl>
2019-10-03 20:20:17 +0200

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+function Counter() {
+    return (
+        <div>
+            <Button sign="-"/>
+            <Button sign="+"/>
+        </div>
+    );
+}
+
+class Button extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <button>{this.props.sign}</button>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Counter/>,
+    document.getElementById('root')
+);
+
+export default Counter;
